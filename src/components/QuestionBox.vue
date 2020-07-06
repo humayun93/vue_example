0f3b3ffd2b991f5d670c3f74cf845e166b3f0595
Humayun Babar <humayun93@users.noreply.github.com>
2020-07-06 17:32:24 +0500

    Added next functionality

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -13,7 +13,7 @@
       </p>
 
       <b-button variant="primary" href="#"> Submit</b-button>
-      <b-button variant="success" href="#"> Next</b-button>
+      <b-button variant="success" href="#" @click="next" > Next</b-button>
     </b-jumbotron>
   </div>
 </template>
@@ -21,7 +21,8 @@
 <script>
 export default {
   props: {
-    currentQuestion: Object
-  }
+    currentQuestion: Object,
+    next: Function
+  },
 }
 </script>
